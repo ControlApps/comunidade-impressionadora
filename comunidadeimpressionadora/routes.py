ff9784acebbe02c135e5ae78366f53314aa8a0b5
llthd <llthdhygg@gmail.com>
2023-07-18 20:29:12 -0400

    first

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -24,19 +24,28 @@ def usuarios():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
-    form_criarconta = FormCriarConta()
     form_resetsenha = FormResetSenha()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
         flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
+    elif form_resetsenha.validate_on_submit() and 'botao_submit_resetsenha' in request.form:
+        # Lógica para redefinir a senha
+        flash(f'Senha redefinida com sucesso no e-mail: {form_resetsenha.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
+    return render_template('login.html', form_login=form_login, form_resetsenha=form_resetsenha)
+
+
+@app.route('/criarconta', methods=['GET', 'POST'])
+def criarconta():
+    form_criarconta = FormCriarConta()
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         existing_user = Usuario.query.filter_by(email=form_criarconta.email.data).first()
         if existing_user:
             flash('O e-mail já está sendo usado por outro usuário.', 'alert-danger')
-            return redirect(url_for('login'))
-
+            return redirect(url_for('criarconta'))
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data,
                           senha=form_criarconta.senha.data)
         database.session.add(usuario)
@@ -44,9 +53,16 @@ def login():
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
 
+    return render_template('criarconta.html', form_criarconta=form_criarconta)
+
+
+@app.route('/resetsenha', methods=['GET', 'POST'])
+def resetsenha():
+    form_resetsenha = FormResetSenha()
+
     if form_resetsenha.validate_on_submit() and 'botao_submit_resetsenha' in request.form:
         # Lógica para redefinir a senha
         flash(f'Senha redefinida com sucesso no e-mail: {form_resetsenha.email.data}', 'alert-success')
         return redirect(url_for('home'))
 
-    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta, form_resetsenha=form_resetsenha)
+    return render_template('resetsenha.html', form_resetsenha=form_resetsenha)
